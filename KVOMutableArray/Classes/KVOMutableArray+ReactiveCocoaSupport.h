51111fd58d86e36c753edead19418ec14c3db5a0
wuw <fifyrio@qq.com>
2017-12-29 14:43:39 +0800

    当数组里某个元素发生变化时，就无法监听到该信号，所以增加了这个方法(但是订阅无法拿到整个数组)； 还有就是数组初始化的时候无法获取该信号

--- KVOMutableArray/Classes/KVOMutableArray+ReactiveCocoaSupport.h
+++ KVOMutableArray/Classes/KVOMutableArray+ReactiveCocoaSupport.h
@@ -10,5 +10,20 @@
 
 @class RACSignal;
 @interface KVOMutableArray (ReactiveCocoaSupport)
+
+/**will add--
+ 数组初始化以及增加、减少产生的信号
+ 
+ @return RACSignal
+ */
+- (RACSignal*)initialAndChangeSignal;
+
+/**will add--
+ 数组初始化以及增加、减少、某个元素发生变化产生的信号
+ 
+ @return RACSignal
+ */
+- (RACSignal*)changeSignalContainElements;
+
 - (RACSignal*)changeSignal;
 @end
--- KVOMutableArray/Classes/KVOMutableArray+ReactiveCocoaSupport.m
+++ KVOMutableArray/Classes/KVOMutableArray+ReactiveCocoaSupport.m
@@ -10,12 +10,61 @@
 #import "RACEXTKeyPathCoding.h"
 #import "NSObject+RACPropertySubscribing.h"
 #import "KVOMutableArrayObserver.h"
+#import "RACSignal+Operations.h"
+#import <objc/runtime.h>
+
 
 @interface KVOMutableArray (ReactiveCocoaSupportInternal)
 - (KVOMutableArrayObserver*)observer;
 @end
 
 @implementation KVOMutableArray (ReactiveCocoaSupport)
+
+@implementation KVOMutableArray (ReactiveCocoaSupport)
+
+
+/**
+ 数组初始化以及增加、减少产生的信号
+
+ @return RACSignal
+ */
+- (RACSignal*)initialAndChangeSignal{
+    KVOMutableArrayObserver* observer = [self observer];
+    RACSignal* signal = [observer rac_valuesAndChangesForKeyPath:@keypath(observer, arr)
+                                                         options:NSKeyValueObservingOptionInitial|NSKeyValueObservingOptionNew
+                                                        observer:observer];       
+    return mergedSignal;
+}
+
+
+/**
+ 数组初始化以及增加、减少、某个元素发生变化产生的信号
+
+ @return RACSignal
+ */
+- (RACSignal*)changeSignalContainElements{
+    KVOMutableArrayObserver* observer = [self observer];
+    RACSignal* signal = [observer rac_valuesAndChangesForKeyPath:@keypath(observer, arr)
+                                                         options:NSKeyValueObservingOptionInitial|NSKeyValueObservingOptionNew
+                                                        observer:observer];
+    
+    NSMutableArray *signals = @[].mutableCopy;
+    [signals addObject:signal];
+    
+    [observer.arr enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        unsigned int count;
+        objc_property_t *propertyList = class_copyPropertyList([obj class], &count);
+        for (unsigned int i = 0; i<count; i++) {
+            const char *propertyName = property_getName(propertyList[i]);
+            NSString *path = [NSString stringWithUTF8String:propertyName];
+            RACSignal *subSignal = [obj rac_valuesAndChangesForKeyPath:path options:NSKeyValueObservingOptionNew observer:nil];
+            [signals addObject:subSignal];
+        }
+    }];
+    RACSignal *mergedSignal = [RACSignal merge:signals.copy];
+    return mergedSignal;
+}
 
 - (RACSignal*)changeSignal
 {
